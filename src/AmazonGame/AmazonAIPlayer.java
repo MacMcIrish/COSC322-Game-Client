3369ff7d67c52099635acea5132c3ebbed3c1ff7
Paul Myhr <Goreng@Pauls-MacBook-Pro.local>
2017-03-27 12:17:14 -0700

    set ai player to minimax

--- src/AmazonGame/AmazonAIPlayer.java
+++ src/AmazonGame/AmazonAIPlayer.java
@@ -283,9 +283,9 @@ public class AmazonAIPlayer extends AmazonPlayer {
         if (args.length != 0) evaluator = Integer.parseInt(args[0]);
 
         //TODO: replace this with a window that will allow you to select a different player
-        AmazonAIPlayer p1 = new AmazonAIPlayer(uuid, uuid, new BestMobilityEvaluator());
+        AmazonAIPlayer p1 = new AmazonAIPlayer(uuid, uuid, evaluators[evaluator]);
         //AmazonAIPlayer p2 = new AmazonAIPlayer(uuid + "2", uuid + "2", evaluators, new double[] {0.1,0.3,0.6});
-        AmazonAIPlayer p3 = new AmazonAIPlayer(uuid+"3", uuid+"3", new BestMobilityEvaluator());
+        //AmazonAIPlayer p3 = new AmazonAIPlayer(uuid+"3", uuid+"3", new BestMobilityEvaluator());
     }
 
     /**
